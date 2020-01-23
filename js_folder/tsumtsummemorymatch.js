b90e06ef22ef65963992b45ea7c52a3823326eab
Noel Carino <leononirac1992@gmail.com>
2020-01-23 14:27:25 -0800

    1/23 | Create a basic header for testing

--- js_folder/tsumtsummemorymatch.js
+++ js_folder/tsumtsummemorymatch.js
@@ -5,6 +5,23 @@ class TsumTsumMemoryMatch{
         }
     }
     loadPage(){
-        console.log("Page Loaded :D");
+        this.loadHeader();
+    }
+    loadHeader(){
+        var liLeaderBoard = $("<li>")
+                        .attr("id","liLeaderBoard")
+                        .addClass("liNav")
+                        .text("LeaderBoard");
+
+        var liHome = $("<li>")
+                        .attr("id","liHome")
+                        .addClass("liNav")
+                        .text("Home");
+
+        var ulHeader = $("<ul>")
+                        .attr("id","ulHeader")
+                        .append(liHome,liLeaderBoard);
+
+        ulHeader.appendTo(this.dom.container);
     }
 }
